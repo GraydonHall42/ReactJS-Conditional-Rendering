79f37e98e9ca666dc59c2cc4eaf54c18fe7ade6f
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-22 13:40:09 -0700

    practiced using turnary and && operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import Login from "./Login.jsx";
 
 var isLoggedIn = false;
 
-function renderConditionally() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return (
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    );
-  }
-}
+// function renderConditionally() {
+//   if (isLoggedIn === true) {
+//     return <h1>Hello</h1>;
+//   } else {
+//     return <Login />;
+//   }
+// }
+
+const currentTime = new Date().getHours();
 
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    // <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+
+    currentTime > 12 && <h1>Why are you still working?</h1>
+  );
 }
 
 export default App;
